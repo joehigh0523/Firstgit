23c48835dd886a6b2f46707396bd0f024780a820
joehigh0523 <joe_high@hotmail.com>
2020-09-10 18:07:02 +0800

    add init ch

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from tkinter import *
 from tkinter import ttk
 from testtry import *
 from IQmeasurePyWrapper import *
+import numpy as np
 import subprocess
 # -----create window----- #
-
+# test git
 
 class port_sel:
     VSAP = 0
@@ -28,9 +29,23 @@ class Ace_python_GUI:
         self.Py_sample.minsize(1024, 800)
         #TC = TESTER_SETUP()
         #IQC = IQ_CONTROL()
-        self.List2G = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
-         '12', '13', '14']
-        self.List5G = ['1', '2']
+        # init chl
+        self.List5G = []
+        self.List2G = []
+        for chlval in range(1, 202, 1):
+            self.List5G.append(chlval)
+            if chlval < 13:
+                self.List2G.append(chlval)
+
+        listch = self.List2G
+        def selch(*args):
+            sel = chlsel.get()
+            if sel == freqband[0]:
+                listch = self.List2G
+            elif sel == freqband[1]:
+                listch = self.List5G
+            self.cmbx5.config(value=listch)
+
         #   frame create
         testfrm = LabelFrame(self.Py_sample,text="Output", width=1024, height=400, bg="gray60", bd=5, relief=SUNKEN)
         testfrm.pack()
@@ -60,7 +75,7 @@ class Ace_python_GUI:
         self.rout_val = IntVar()
         self.rout_val.set(0)
         #
-        for value , item in Rout:
+        for value, item in Rout:
             rout_bt = Radiobutton(Testerip_frame2, text = item, variable=self.rout_val, value=value, bg='white')
             rout_bt.grid(column=value, row=0 )
 
@@ -68,13 +83,16 @@ class Ace_python_GUI:
         Testerframe4 = LabelFrame(testfrm2, text="Select Frequency and channel", font=("Times", 10, "normal"), bg="khaki1",
                                      padx=40, pady=5)
         Testerframe4.place(x=260, y=70)
-
-        self.cmbx4 = ttk.Combobox(Testerframe4, width=10)
+        #
+        freqband = ['2.4G','5G']
+        chlsel = StringVar()
+        chlsel.set('sel band')
+        self.cmbx4 = ttk.Combobox(Testerframe4, textvariable=chlsel, value=freqband, width=10)
+        self.cmbx4.bind("<<ComboboxSelected>>", selch)
         self.cmbx4.grid(column=0, row=0)
-        self.cmbx4['value'] = ('2.4G', '5G')
-        self.cmbx4.bind("<<select>>", self.combbpost)
-        self.cmbx5 = ttk.Combobox(Testerframe4, value=self.List2G, width=10)
+        self.cmbx5 = ttk.Combobox(Testerframe4, value=listch, width=10)
         self.cmbx5.grid(column=1, row=0)
+
        # if self.combbpost() != "5G":
           # self.cmbx5['value'] = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
                         #           '12', '13', '14')
@@ -127,17 +145,6 @@ class Ace_python_GUI:
 
         subprocess.Popen(['echo', 'Try execute sub command or program'], shell=False)
         self.Py_sample.mainloop()
-
-
-    def combbpost(self):
-        setfreq = self.cmbx4.get()
-        print(setfreq)
-        if setfreq != '5G':
-            tmplist = self.List2G
-        else:
-            tmplist = self.List5G
-        self.cmbx5.config(value=tmplist)
-
 
     def GetIP(self):
         #if self.rout1.get():
